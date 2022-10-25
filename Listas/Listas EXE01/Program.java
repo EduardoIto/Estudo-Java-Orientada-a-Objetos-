5c01e870c50376e8c6f0ad75a619eba53344917a
Eduardo Ito <73322050+EduardoIto@users.noreply.github.com>
2022-10-25 08:45:00 -0300

    Create Program.java

---
+++ Listas/Listas EXE01/Program.java
@@ -0,0 +1,91 @@
+/*
+A dona de um pensionato possui dez quartos para alugar para estudantes,
+sendo esses quartos identificados pelos números 0 a 9.
+Fazer um programa que inicie com todos os dez quartos vazios, e depois
+leia uma quantidade N representando o número de estudantes que vão
+alugar quartos (N pode ser de 1 a 10). Em seguida, registre o aluguel dos
+N estudantes. Para cada registro de aluguel, informar o nome e email do
+estudante, bem como qual dos quartos ele escolheu (de 0 a 9). Suponha
+que seja escolhido um quarto vago. Ao final, seu programa deve imprimir
+um relatório de todas ocupações do pensionato, por ordem de quarto,
+conforme exemplo.
+
+How many rooms will be rented? 3
+Rent #1:
+Name: Maria Green
+Email: maria@gmail.com
+Room: 5
+Rent #2:
+Name: Marco Antonio
+Email: marco@gmail.com
+Room: 1
+Rent #3:
+Name: Alex Brown
+Email: alex@gmail.com
+Room: 8
+Busy rooms:
+1: Marco Antonio, marco@gmail.com
+5: Maria Green, maria@gmail.com
+8: Alex Brown, alex@gmail.com
+
+--------------------------------------------------------------------------
+*/
+
+package application;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Employee;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("How many employees will be registered?: ");
+		int n = sc.nextInt();
+		
+		List<Employee> list = new ArrayList<>();
+		
+		for(int i = 0; i < n; i++) {
+			System.out.println("EMPLOYEE #" + (i + 1) + ":");
+			System.out.print("Id: ");
+			Integer id = sc.nextInt();
+			sc.nextLine();
+			System.out.print("Name: ");
+			String name = sc.nextLine();
+			System.out.print("Salary: ");
+			Double salary = sc.nextDouble();
+			list.add(new Employee(id, name, salary));
+			System.out.println();
+		}
+		
+		System.out.println();
+		System.out.print("Enter the employee id that will have salary increase: ");
+		int id = sc.nextInt();
+		Employee emp = list.stream().filter(x -> x.getId() == id).findFirst().orElse(null);
+		if(emp == null) {
+			System.out.println("this is does not exist!");
+		}
+		else {
+			System.out.print("Enter the percentage: ");
+			double percent = sc.nextDouble();
+			emp.increaseSalary(percent);
+		}
+		
+		System.out.println();
+		System.out.println("List of employees: ");
+		for(Employee obj : list) {
+			System.out.println(obj);
+		}
+				
+		sc.close();
+
+	}
+
+}
+
+
